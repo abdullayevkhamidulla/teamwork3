f3b604abec228ad2bb944ad74c886945f57888c6
hamidullo <ahamidullo248@gmail.com>
2026-01-28 14:21:30 +0500

    navbar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,15 @@ const Navbar = () => {
   return (
     <div>
       <h1>Navbar</h1>
+      <nav>
+        <ul>
+            <li>
+                <a>Home</a>
+                <a>Home</a>
+                <a>Home</a>
+            </li>
+        </ul>
+      </nav>
     </div>
   )
 }
